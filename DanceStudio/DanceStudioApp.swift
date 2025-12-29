8cecdb400f20c391a24db27c5b274893ce4ce028
Ghulam Abbas Wafa <Abbas.wafa2023@gmail.com>
2025-12-29 12:55:53 +0500

    Initial Commit

---
+++ DanceStudio/DanceStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanceStudioApp.swift
+//  DanceStudio
+//
+//  Created by Ghulam Abbas Wafa on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DanceStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
